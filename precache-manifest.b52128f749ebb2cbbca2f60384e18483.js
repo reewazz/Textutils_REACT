9ed652c415ca8c3fc4dddc7ab1e77ac5fabf7846
reewazz <85682819+reewazz@users.noreply.github.com>
2022-01-26 21:46:20 +0545

    Updates

---
+++ precache-manifest.b52128f749ebb2cbbca2f60384e18483.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60e41751248824eb632aeb4d83cf035d",
+    "url": "/Textutils_REACT/index.html"
+  },
+  {
+    "revision": "1521ebfb2a079e7d4973",
+    "url": "/Textutils_REACT/static/css/main.ce208ee1.chunk.css"
+  },
+  {
+    "revision": "36bbcc1c64e7cfc6e1f6",
+    "url": "/Textutils_REACT/static/js/2.7a0df4c3.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/Textutils_REACT/static/js/2.7a0df4c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b4b86211a7864b5010a",
+    "url": "/Textutils_REACT/static/js/3.34b09613.chunk.js"
+  },
+  {
+    "revision": "1521ebfb2a079e7d4973",
+    "url": "/Textutils_REACT/static/js/main.05052d05.chunk.js"
+  },
+  {
+    "revision": "8b6e5aa3fd7f89a91fa8",
+    "url": "/Textutils_REACT/static/js/runtime-main.fb83071a.js"
+  }
+]);
